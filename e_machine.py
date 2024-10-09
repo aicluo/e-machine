b6408bb624f84452f9ef215d07d41a3bfb96e280
Alicia Luo <aicluo@gmail.com>
2024-10-09 11:29:07 -0700

    I don't even remember what I changed

--- e_machine.py
+++ e_machine.py
@@ -1,20 +1,19 @@
 import numpy as np
 import math
 
+def e_machine(states, transition_matrix, n: int):
+    """
+        Generate a sequence of outputs and corresponding states based on labeled transition matrices
 
-"""
-    Generate a sequence of outputs and corresponding states based on provided probability matrices
+        Parameters:
+            states (list): list of state names as strings, same order as probability matrices
+                        (ex: ["A", "B", "C"])
+            transition_matrix: see transition_encoding.jpg
+            n (int): length of sequence to output
 
-    Parameters:
-    states (list): list of state names as strings, same order as probability matrices
-                   (ex: ["A", "B", "C"])
-    transition_matrix: see above image
-    n (int): length of sequence to output
-
-    Returns:
-    two lists, one with the outputs and the other with the corresponding states
-"""
-def e_machine(states, transition_matrix, n):
+        Returns:
+            two lists, one with the outputs and the other with the corresponding states
+    """
     output_states = []
     outputs = []
 
@@ -39,7 +38,17 @@ def e_machine(states, transition_matrix, n):
     return output_states, outputs
 
 
-def probability_rederivation(output_states, outputs):
+def probability_rederivation(output_states: list, outputs: list):
+    """
+        Rederive the labelled transition matrices based on output and corresponding states
+
+        Parameters:
+            output_states (list): list of states corresponding to outputs
+            outputs (list): list of outputs generated from labeled transition matrices
+
+        Returns:
+            dictionary whose keys are the transitions and whose values are the transition probabilities
+    """
     transitions = {}
     for i, output in enumerate(outputs):
         key = f"({output_states[i+1]}, {output} | {output_states[i]})"
@@ -59,23 +68,45 @@ def probability_rederivation(output_states, outputs):
     
     return probabilities
 
-def distribution_generator(num_states, num_outputs):
+def distribution_generator(num_states: int, num_outputs: int):
+    """
+    Generate random distribution to be used as labeled transition matrices
+    """
     alpha = [1] * num_states * num_outputs
     return np.random.dirichlet(alpha, num_states)
 
 def reservoir(h_t, x_t, W, v):
+    """
+    Reservoir with equation h_{t+1} = tanh(W*h_t + v*x_t)
+    """
     return np.tanh(W @ h_t + v*x_t)
 
-def weight_initialization(m, n, epsilon):
+
+def weight_initialization(n, epsilon=0.1):
+    """
+    Initializes weight matrices
+    Weight matrix of hidden input has eigenvalues between -1 and 1 and mean 0
+
+    Parameters:
+        n (int): size of weight matrix
+        epsilon (float): marginal quantity to make sure the matrix's eigenvalues are between -1 and 1
+
+    Returns:
+        W and v, the weight matrix of the hidden input (size [n, n]) and the weight matrix of the external input
+"""
     # generate random matrix
-    W = np.random.uniform(low = -1, high = 1, size=[m,n])
+    W = np.random.uniform(low = -1, high = 1, size=[n,n])
 
-    # find largest magnitude eigenvalue
-    eig = max(abs(np.linalg.eigvals(W) + epsilon))
-    if eig > 1:
-        W = W/eig
+    if n > 1:
+        # find largest magnitude eigenvalue
+        eig = max(abs(np.linalg.eigvals(W)))
+        if eig > 1:
+            W = W/(eig + epsilon)
 
-    # adjust mean to 0
-    W = W - np.mean(W)
+        # adjust mean to 0
+        W = W - np.mean(W)
     
-    return W
+    # generate v
+    v = np.random.rand(n,1)
+
+    return W, v
